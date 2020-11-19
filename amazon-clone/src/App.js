67ef8f23c7f4fa84a2368ef551542eb22269e08d
Shubham Rajput <ShubhamBinary01@github.com>
2020-11-19 12:41:22 +0530

    Created React App And Setup Firebase

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from'react';
+import './App.css';
+
+function App() {
+  return (
+    //BEM  Block, Element, Modifier methodology (commonly referred to as BEM)
+    <div className="App">
+      <h1>Hello Developers, let's build Amazon Clone </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCDdKXvE4zxhNiGBH62TKnzRISoqzGADMw",
+    authDomain: "challenge-bd1d2.firebaseapp.com",
+    databaseURL: "https://challenge-bd1d2.firebaseio.com",
+    projectId: "challenge-bd1d2",
+    storageBucket: "challenge-bd1d2.appspot.com",
+    messagingSenderId: "585154904662",
+    appId: "1:585154904662:web:789ad6dc4b140722f971f7",
+    measurementId: "G-M4QY2N0NHY"
+  };
